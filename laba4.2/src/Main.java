d65a64b9207e7eca775e356e75b3a2bc8c884aa7
IskanderioBik <151126425+IskanderioBik@users.noreply.github.com>
2025-09-24 18:51:26 +0300

    Add files via upload

---
+++ laba4.2/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> january = new ArrayList<>();
+        january.add(1);
+        january.add(7);
+        ArrayList<Integer> february = new ArrayList<>();
+        february.add(23);
+        ArrayList<Integer> march = new ArrayList<>();
+        march.add(8);
+        ArrayList<Integer> may = new ArrayList<>();
+        may.add(1);
+        may.add(9);
+        ArrayList<Integer> june = new ArrayList<>();
+        june.add(12);
+        ArrayList<Integer> november = new ArrayList<>();
+        november.add(4);
+
+        HashMap<String, ArrayList<Integer>> stateHolidays = new HashMap<>();
+        stateHolidays.put("Январь", january);
+        stateHolidays.put("Февраль", february);
+        stateHolidays.put("Март", march);
+        stateHolidays.put("Май", may);
+        stateHolidays.put("Июнь", june);
+        stateHolidays.put("Ноябрь", november);
+        for (String month : stateHolidays.keySet()) {
+            System.out.println(month + ": " + stateHolidays.get(month));
+        }
+    }
+}
